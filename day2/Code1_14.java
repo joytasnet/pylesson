b9a3a6c1134b9754c02afd4e78eab6117ea4d09e
mjpurin <mjpurin@gmail.com>
2021-12-24 11:03:34 +0900

    c1_14,Code1_14作成 pythonでのキーボード入力と Javaでの入力の違いを体験

---
+++ day2/Code1_14.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Code1_14{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("あなたの名前を入力>>");
+		String name=sc.nextLine();
+		System.out.printf("おお!%sが来るのを待っておったぞ!",name);
+	}
+}
