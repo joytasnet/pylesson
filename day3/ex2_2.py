b33745ae73f6451c1a0cecee4ecad7b6d71460b7
mjpurin <mjpurin@gmail.com>
2021-12-28 11:42:53 +0900

    Ex2_2変更 JSだった場合の処理追加

--- day3/ex2_2.py
+++ day3/ex2_2.py
@@ -4,4 +4,15 @@ li.append(int(input('算数>>')))
 li.append(int(input('理科>>')))
 li.append(int(input('社会>>')))
 li.append(int(input('英語>>')))
+
+"""
+let li=[];
+li.push(10);
+li.push(20);
+let sum=0;
+for(let i=0;i<li.length;i++){
+    sum+=li[i];
+}
+console.log(`合計点${sum},平均点${sum/li.length}`)
+"""
 print(f'合計点{sum(li)},平均点{sum(li)/len(li)}')
